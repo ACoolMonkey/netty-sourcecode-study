e399eb5425ea3527a0a1dbda565cf02eaebc2599
Robert Hou <houyishuang950215@qq.com>
2022-05-16 16:40:02 +0800

    Revert "update"
    This reverts commit a09a650716a51285af4595384994e7b91561ce68.

--- common/src/main/java/io/netty/util/concurrent/DefaultEventExecutorChooserFactory.java
+++ common/src/main/java/io/netty/util/concurrent/DefaultEventExecutorChooserFactory.java
@@ -28,17 +28,10 @@ public final class DefaultEventExecutorChooserFactory implements EventExecutorCh
 
     public static final DefaultEventExecutorChooserFactory INSTANCE = new DefaultEventExecutorChooserFactory();
 
-    private DefaultEventExecutorChooserFactory() {
-    }
+    private DefaultEventExecutorChooserFactory() { }
 
     @Override
     public EventExecutorChooser newChooser(EventExecutor[] executors) {
-        /*
-        这里会根据executors的长度是否是2的幂来选择调用PowerOfTwoEventExecutorChooser
-        或GenericEventExecutorChooser。之所以有这个区别是因为后续会调用到next方法，
-        而next方法会通过取余的方式来找到下一个EventExecutor。而如果长度为2的幂的话，会有
-        优化的写法而不是使用默认的“%”取余符号，后续会详细讲解
-         */
         if (isPowerOfTwo(executors.length)) {
             return new PowerOfTwoEventExecutorChooser(executors);
         } else {
@@ -46,15 +39,6 @@ public final class DefaultEventExecutorChooserFactory implements EventExecutorCh
         }
     }
 
-    /**
-     * 该方法是用来判断val是否是2的幂。一个数的负数和原数的二进制形式的区别是取反再+1
-     * 所以只有val是2的幂的时候，这个数和它取负数的数在最后val.length个位数长度上的
-     * 数是完全一样的。所以两者按位与的结果就是这个数，以此就可以判断出来是否是2的幂了
-     * 举个例子：假如说val是3，二进制是11。而-3的二进制是1111 1111 1111 1111 1111 1111 1111 1101
-     * 两者在最后两位分别是11和01，是不一样的，所以按位与的结果肯定不是11，也就是十进制的3；
-     * 那么假如说val是4，二进制是100；而-4的二进制是1111 1111 1111 1111 1111 1111 1111 1100
-     * 两者在最后三位都是100，所以按位与的结果就是100，也就是十进制的4，也就是判断是相等的
-     */
     private static boolean isPowerOfTwo(int val) {
         return (val & -val) == val;
     }
@@ -64,7 +48,6 @@ public final class DefaultEventExecutorChooserFactory implements EventExecutorCh
         private final EventExecutor[] executors;
 
         PowerOfTwoEventExecutorChooser(EventExecutor[] executors) {
-            //这里将children赋值进PowerOfTwoEventExecutorChooser的executors
             this.executors = executors;
         }
 
@@ -82,7 +65,6 @@ public final class DefaultEventExecutorChooserFactory implements EventExecutorCh
         private final EventExecutor[] executors;
 
         GenericEventExecutorChooser(EventExecutor[] executors) {
-            //这里将children赋值进GenericEventExecutorChooser的executors
             this.executors = executors;
         }
 
